7ca93b49f2087b4e0bedd6df8454cced95377c60
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2023-03-12 01:47:26 +0700

    update

--- Java/PBO/Buku.java
+++ Java/PBO/Buku.java
@@ -29,6 +29,8 @@ public class Buku {
         sinopsis = "";
         for (String s : kalimat){
             if (!s.isEmpty()){
+                s = s.trim();
+                s = s.replaceAll("\\s+"," ");
                 sinopsis += Character.toUpperCase(s.charAt(0)) + s.substring(1) + " ";
             }
         }
@@ -39,34 +41,50 @@ public class Buku {
         return kata.length;
     }
 
-    public double jumlahRoyalti(double penjualan){
+    public String jumlahRoyalti(double penjualan){
         double royalti = penjualan * harga / 10;
-        return royalti;
+        return df.format(royalti);
     }
 
-    public double jumlahRoyalti(double penjualan, double persentase){
+    public String jumlahRoyalti(double penjualan, double persentase){
         if (persentase>1) persentase /= 100;
         double royalti = penjualan * harga * persentase;
-        return royalti;
+        return df.format(royalti);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList <Buku> kumpulanBuku = new ArrayList<Buku>();
+        // ArrayList<Buku> kumpulanBuku = new ArrayList<Buku>();
         int jumlahBuku = in.nextInt(); in.nextLine();
+        Buku[] kumpulanBuku = new Buku[jumlahBuku];
+        double[] penjualan = new double[jumlahBuku];
+        double[] persentase = new double[jumlahBuku];
         for (int i = 0; i < jumlahBuku; i++) {
             String sinopsis = in.nextLine();
-            double harga = in.nextDouble();
-            Buku buku = new Buku(sinopsis, harga);
-            kumpulanBuku.add(buku);
+            double harga = in.nextDouble(); in.nextLine();
+            String input = in.nextLine();
+            String[] params = input.split(" ");
+            penjualan[i]  = Double.parseDouble(params[0]);
+            persentase[i] = 0;
+            if (params.length > 1) {
+                persentase[i] = Double.parseDouble(params[1]);
+            }
+            kumpulanBuku[i] = new Buku(sinopsis, harga);
         }
-        for (int i = 0; i < kumpulanBuku.size(); i++) {
-            Buku buku = kumpulanBuku.get(i);
+        for (int i = 0; i < kumpulanBuku.length; i++) {
+            Buku buku = kumpulanBuku[i];
             buku.perbaikiSinopsis();
+            System.out.printf("\nBuku %d : \n",i+1);
             System.out.printf("Sinopsis\t: %s\n",buku.getSinopsis());
-            System.out.printf("Harga\t\t: Rp.%s\n",buku.getHarga());
             System.out.printf("Jumlah Kata\t: %d\n",buku.jumlahKata());
+            System.out.printf("Harga\t\t: Rp.%s\n",buku.getHarga());
+            System.out.printf("Terjual\t\t: %.0f Unit\n",penjualan[i]);
+            if (persentase[i]!=0){
+                System.out.printf("Royalti %.0f%%\t: Rp.%s\n",persentase[i],buku.jumlahRoyalti(penjualan[i],persentase[i]));
+            } else {
+                System.out.printf("Royalti 10%%\t: Rp.%s\n",buku.jumlahRoyalti(penjualan[i]));
+            }
         }
-
+        System.out.println();
     }
 }
