628b0959caa1aca282e5c9bbd44f5d2648cf8ea5
Gustav <mgustav.penting@gmail.com>
2023-05-22 15:29:26 +0700

    refactor

--- Java/PBO/Praktikum/Balls/Main.java
+++ Java/PBO/Praktikum/Balls/Main.java
@@ -58,155 +58,141 @@ public class Main {
             beratMapB.put(pemain.berat, pemain);
         }
 
+        findPlayersWithSameHeight(timA, timB);
+        findPlayersWithSameWeight(timA, timB);
+
+        findPlayersWithUniqueHeight(timA, timB);
+        findPlayersWithUniqueWeight(timA, timB);
+
+        findPlayersWithUniqueHeight(timB, timA);
+        findPlayersWithUniqueWeight(timB, timA);
+        
+        analyzePlayerHeight(timA, "A");
+        analyzePlayerHeight(timB, "B");
+
+        analyzePlayerWeight(timA, "A");
+        analyzePlayerWeight(timB, "B");
+
+    }
+
+    public static void analyzePlayerHeight(List<Pemain> team, String teamName) {
+        System.out.println("\nKategori Range Tinggi di Tim " + teamName + ":");
+        int minRangeTinggi = Collections.min(team, Comparator.comparing(p -> p.tinggi)).tinggi;
+        int maxRangeTinggi = Collections.max(team, Comparator.comparing(p -> p.tinggi)).tinggi;
+        System.out.println("Tinggi Min: " + minRangeTinggi + " cm");
+        System.out.println("Tinggi Max: " + maxRangeTinggi + " cm");
+
+        TreeMap<Integer, List<Pemain>> rangeTinggi = new TreeMap<>();
+        for (Pemain pemain : team) {
+            int tinggiGroup = pemain.tinggi / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 tinggi badan
+            if (!rangeTinggi.containsKey(tinggiGroup)) {
+                rangeTinggi.put(tinggiGroup, new ArrayList<>());
+            }
+            rangeTinggi.get(tinggiGroup).add(pemain);
+        }
+
+        for (Map.Entry<Integer, List<Pemain>> entry : rangeTinggi.entrySet()) {
+            int tinggiGroup = entry.getKey();
+            List<Pemain> pemainList = entry.getValue();
+            int minTinggi = Collections.min(pemainList, Comparator.comparing(p -> p.tinggi)).tinggi;
+            int maxTinggi = Collections.max(pemainList, Comparator.comparing(p -> p.tinggi)).tinggi;
+            System.out.printf("Range %d - %d cm:%n", tinggiGroup, (tinggiGroup + 9));
+            System.out.printf("Min: %d cm, Max: %d cm%n", minTinggi, maxTinggi);
+            for (Pemain pemain : pemainList) {
+                System.out.printf("- %d (%d cm)%n", pemain.noPemain, pemain.tinggi);
+            }
+        }
+    }
+
+    public static void analyzePlayerWeight(List<Pemain> team, String teamName) {
+        System.out.println("\nKategori Range Berat di Tim " + teamName + ":");
+        int minRangeBerat = Collections.min(team, Comparator.comparing(p -> p.berat)).berat;
+        int maxRangeBerat = Collections.max(team, Comparator.comparing(p -> p.berat)).berat;
+        System.out.println("Berat Min: " + minRangeBerat + " kg");
+        System.out.println("Berat Max: " + maxRangeBerat + " kg");
+
+        TreeMap<Integer, List<Pemain>> rangeBerat = new TreeMap<>();
+        for (Pemain pemain : team) {
+            int beratGroup = pemain.berat / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 berat badan
+            if (!rangeBerat.containsKey(beratGroup)) {
+                rangeBerat.put(beratGroup, new ArrayList<>());
+            }
+            rangeBerat.get(beratGroup).add(pemain);
+        }
+
+        for (Map.Entry<Integer, List<Pemain>> entry : rangeBerat.entrySet()) {
+            int beratGroup = entry.getKey();
+            List<Pemain> pemainList = entry.getValue();
+            int minBerat = Collections.min(pemainList, Comparator.comparing(p -> p.berat)).berat;
+            int maxBerat = Collections.max(pemainList, Comparator.comparing(p -> p.berat)).berat;
+            System.out.printf("Range %d - %d kg:%n", beratGroup, (beratGroup + 9));
+            System.out.printf("Min: %d kg, Max: %d kg%n", minBerat, maxBerat);
+            for (Pemain pemain : pemainList) {
+                System.out.printf("- %d (%d kg)%n", pemain.noPemain, pemain.berat);
+            }
+        }
+    }
+    private static void findPlayersWithSameHeight(List<Pemain> teamA, List<Pemain> teamB) {
+        Map<Integer, Pemain> heightMap = createHeightMap(teamA);
         System.out.println("Tinggi badan yang sama:");
-        for (Pemain pemain : timB) {
-            if (tinggiMap.containsKey(pemain.tinggi)) {
-                Pemain pemainTimA = tinggiMap.get(pemain.tinggi);
+        for (Pemain pemain : teamB) {
+            if (heightMap.containsKey(pemain.tinggi)) {
+                Pemain pemainTimA = heightMap.get(pemain.tinggi);
                 System.out.println("Pemain dengan tinggi " + pemain.tinggi +
-                        " ditemukan di kedua tim (Tim A Pemain No dan berat : " + pemainTimA.noPemain + ", " + pemainTimA.berat + "kg, Tim B Pemain No dan berat : " + pemain.noPemain + ", " + pemain.berat + "kg)");
+                        " ditemukan di kedua tim (Tim A Pemain No dan berat : " + pemainTimA.noPemain +
+                        ", " + pemainTimA.berat + "kg, Tim B Pemain No dan berat : " +
+                        pemain.noPemain + ", " + pemain.berat + "kg)");
             }
         }
+    }
 
+    private static void findPlayersWithSameWeight(List<Pemain> teamA, List<Pemain> teamB) {
+        Map<Integer, Pemain> weightMap = createWeightMap(teamA);
         System.out.println("\nBerat badan yang sama:");
-        for (Pemain pemain : timB) {
-            if (beratMap.containsKey(pemain.berat)) {
-                Pemain pemainTimA = beratMap.get(pemain.berat);
+        for (Pemain pemain : teamB) {
+            if (weightMap.containsKey(pemain.berat)) {
+                Pemain pemainTimA = weightMap.get(pemain.berat);
                 System.out.println("Pemain dengan berat " + pemain.berat +
-                        " ditemukan di kedua tim (Tim A Pemain No dan tinggi : " + pemainTimA.noPemain + ", " + pemainTimA.tinggi + "cm, Tim B Pemain No dan tinggi : " + pemain.noPemain + ", " + pemain.tinggi + "cm)");
+                        " ditemukan di kedua tim (Tim A Pemain No dan tinggi : " +
+                        pemainTimA.noPemain + ", " + pemainTimA.tinggi + "cm, Tim B Pemain No dan tinggi : " +
+                        pemain.noPemain + ", " + pemain.tinggi + "cm)");
             }
         }
+    }
 
+    private static void findPlayersWithUniqueHeight(List<Pemain> teamA, List<Pemain> teamB) {
+        Map<Integer, Pemain> heightMapB = createHeightMap(teamB);
         System.out.println("\nKomplemen tinggi badan tim A:");
-        for (Pemain pemain : timA) {
-            if (!tinggiMapB.containsKey(pemain.tinggi)) {
+        for (Pemain pemain : teamA) {
+            if (!heightMapB.containsKey(pemain.tinggi)) {
                 System.out.println("Pemain dengan tinggi " + pemain.tinggi + " hanya ada di Tim A");
             }
         }
+    }
 
+    private static void findPlayersWithUniqueWeight(List<Pemain> teamA, List<Pemain> teamB) {
+        Map<Integer, Pemain> weightMapB = createWeightMap(teamB);
         System.out.println("\nKomplemen berat badan tim A:");
-        for (Pemain pemain : timA) {
-            if (!beratMapB.containsKey(pemain.berat)) {
+        for (Pemain pemain : teamA) {
+            if (!weightMapB.containsKey(pemain.berat)) {
                 System.out.println("Pemain dengan berat " + pemain.berat + " hanya ada di Tim A");
             }
         }
+    }
 
-        System.out.println("\nKomplemen tinggi badan tim B:");
-        for (Pemain pemain : timB) {
-            if (!tinggiMap.containsKey(pemain.tinggi)) {
-                System.out.println("Pemain dengan tinggi " + pemain.tinggi + " hanya ada di Tim B");
-            }
+    private static Map<Integer, Pemain> createHeightMap(List<Pemain> team) {
+        Map<Integer, Pemain> heightMap = new HashMap<>();
+        for (Pemain pemain : team) {
+            heightMap.put(pemain.tinggi, pemain);
         }
+        return heightMap;
+    }
 
-        System.out.println("\nKomplemen berat badan tim B:");
-        for (Pemain pemain : timB) {
-            if (!beratMap.containsKey(pemain.berat)) {
-                System.out.println("Pemain dengan berat " + pemain.berat + " hanya ada di Tim B");
-            }
+    private static Map<Integer, Pemain> createWeightMap(List<Pemain> team) {
+        Map<Integer, Pemain> weightMap = new HashMap<>();
+        for (Pemain pemain : team) {
+            weightMap.put(pemain.berat, pemain);
         }
-        
-        // Kategori range tinggi di Tim A
-        System.out.println("\nKategori Range Tinggi di Tim A:");
-        int minRangeTinggiA = Collections.min(timA, Comparator.comparing((Pemain p) -> p.tinggi)).tinggi;
-        int maxRangeTinggiA = Collections.max(timA, Comparator.comparing((Pemain p) -> p.tinggi)).tinggi;
-        System.out.println("Tinggi Min: " + minRangeTinggiA + " cm");
-        System.out.println("Tinggi Max: " + maxRangeTinggiA + " cm");
-        TreeMap<Integer, List<Pemain>> rangeTinggiTimA = new TreeMap<>();
-        for (Pemain pemain : timA) {
-            int tinggiGroup = pemain.tinggi / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 tinggi badan
-            if (!rangeTinggiTimA.containsKey(tinggiGroup)) {
-                rangeTinggiTimA.put(tinggiGroup, new ArrayList<>());
-            }
-            rangeTinggiTimA.get(tinggiGroup).add(pemain);
-        }
-        for (Map.Entry<Integer, List<Pemain>> entry : rangeTinggiTimA.entrySet()) {
-            int tinggiGroup = entry.getKey();
-            List<Pemain> pemainList = entry.getValue();
-            int minTinggi = Collections.min(pemainList, Comparator.comparing(p -> p.tinggi)).tinggi;
-            int maxTinggi = Collections.max(pemainList, Comparator.comparing(p -> p.tinggi)).tinggi;
-            System.out.printf("Range %d - %d cm:%n", tinggiGroup, (tinggiGroup + 9));
-            System.out.printf("Min: %d cm, Max: %d cm%n", minTinggi, maxTinggi);
-            for (Pemain pemain : pemainList) {
-                System.out.printf("- %d (%d cm)%n", pemain.noPemain, pemain.tinggi);
-            }
-        }
-
-        // Kategori range tinggi di Tim B
-        System.out.println("\nKategori Range Tinggi di Tim B:");
-        int minRangeTinggiB = Collections.min(timB, Comparator.comparing((Pemain p) -> p.tinggi)).tinggi;
-        int maxRangeTinggiB = Collections.max(timB, Comparator.comparing((Pemain p) -> p.tinggi)).tinggi;
-        System.out.println("Tinggi Min: " + minRangeTinggiB + " cm");
-        System.out.println("Tinggi Max: " + maxRangeTinggiB + " cm");
-        TreeMap<Integer, List<Pemain>> rangeTinggiTimB = new TreeMap<>();
-        for (Pemain pemain : timB) {
-            int tinggiGroup = pemain.tinggi / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 tinggi badan
-            if (!rangeTinggiTimB.containsKey(tinggiGroup)) {
-                rangeTinggiTimB.put(tinggiGroup, new ArrayList<>());
-            }
-            rangeTinggiTimB.get(tinggiGroup).add(pemain);
-        }
-        for (Map.Entry<Integer, List<Pemain>> entry : rangeTinggiTimB.entrySet()) {
-            int tinggiGroup = entry.getKey();
-            List<Pemain> pemainList = entry.getValue();
-            int minTinggi = Collections.min(pemainList, Comparator.comparing(p -> p.tinggi)).tinggi;
-            int maxTinggi = Collections.max(pemainList, Comparator.comparing(p -> p.tinggi)).tinggi;
-            System.out.printf("Range %d - %d cm:%n", tinggiGroup, (tinggiGroup + 9));
-            System.out.printf("Min: %d cm, Max: %d cm%n", minTinggi, maxTinggi);
-            for (Pemain pemain : pemainList) {
-                System.out.printf("- %d (%d cm)%n", pemain.noPemain, pemain.tinggi);
-            }
-        }
-
-        // Kategori range berat di Tim A
-        System.out.println("\nKategori Range Berat di Tim A:");
-        int minRangeBeratA = Collections.min(timA, Comparator.comparing((Pemain p) -> p.berat)).berat;
-        int maxRangeBeratA = Collections.max(timA, Comparator.comparing((Pemain p) -> p.berat)).berat;
-        System.out.println("Berat Min: " + minRangeBeratA + " kg");
-        System.out.println("Berat Max: " + maxRangeBeratA + " kg");
-        TreeMap<Integer, List<Pemain>> rangeBeratTimA = new TreeMap<>();
-        for (Pemain pemain : timA) {
-            int beratGroup = pemain.berat / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 berat badan
-            if (!rangeBeratTimA.containsKey(beratGroup)) {
-                rangeBeratTimA.put(beratGroup, new ArrayList<>());
-            }
-            rangeBeratTimA.get(beratGroup).add(pemain);
-        }
-        for (Map.Entry<Integer, List<Pemain>> entry : rangeBeratTimA.entrySet()) {
-            int beratGroup = entry.getKey();
-            List<Pemain> pemainList = entry.getValue();
-            int minBerat = Collections.min(pemainList, Comparator.comparing(p -> p.berat)).berat;
-            int maxBerat = Collections.max(pemainList, Comparator.comparing(p -> p.berat)).berat;
-            System.out.printf("Range %d - %d kg:%n", beratGroup, (beratGroup + 9));
-            System.out.printf("Min: %d kg, Max: %d kg%n", minBerat, maxBerat);
-            for (Pemain pemain : pemainList) {
-                System.out.printf("- %d (%d kg)%n", pemain.noPemain, pemain.berat);
-            }
-        }
-
-        // Kategori range berat di Tim B
-        System.out.println("\nKategori Range Berat di Tim B:");
-        int minRangeBeratB = Collections.min(timB, Comparator.comparing((Pemain p) -> p.berat)).berat;
-        int maxRangeBeratB = Collections.max(timB, Comparator.comparing((Pemain p) -> p.berat)).berat;
-        System.out.println("Berat Min: " + minRangeBeratB + " kg");
-        System.out.println("Berat Max: " + maxRangeBeratB + " kg");
-        TreeMap<Integer, List<Pemain>> rangeBeratTimB = new TreeMap<>();
-        for (Pemain pemain : timB) {
-            int beratGroup = pemain.berat / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 berat badan
-            if (!rangeBeratTimB.containsKey(beratGroup)) {
-                rangeBeratTimB.put(beratGroup, new ArrayList<>());
-            }
-            rangeBeratTimB.get(beratGroup).add(pemain);
-        }
-        for (Map.Entry<Integer, List<Pemain>> entry : rangeBeratTimB.entrySet()) {
-            int beratGroup = entry.getKey();
-            List<Pemain> pemainList = entry.getValue();
-            int minBerat = Collections.min(pemainList, Comparator.comparing(p -> p.berat)).berat;
-            int maxBerat = Collections.max(pemainList, Comparator.comparing(p -> p.berat)).berat;
-            System.out.printf("Range %d - %d kg:%n", beratGroup, (beratGroup + 9));
-            System.out.printf("Min: %d kg, Max: %d kg%n", minBerat, maxBerat);
-            for (Pemain pemain : pemainList) {
-                System.out.printf("- %d (%d kg)%n", pemain.noPemain, pemain.berat);
-            }
-        }
-
+        return weightMap;
     }
 }
