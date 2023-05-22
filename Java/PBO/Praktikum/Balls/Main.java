68a93f4219dc09a388ac8d31e1feb7c203e15455
Gustav <mgustav.penting@gmail.com>
2023-05-22 15:58:16 +0700

    List to Map

--- Java/PBO/Praktikum/Balls/Main.java
+++ Java/PBO/Praktikum/Balls/Main.java
@@ -2,6 +2,7 @@ package Java.PBO.Praktikum.Balls;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -11,53 +12,29 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Pemain> timA = new ArrayList<>(
-            Arrays.asList(
-                new Pemain(1, 168, 50),
-                new Pemain(2, 170, 60),
-                new Pemain(3, 165, 56),
-                new Pemain(4, 168, 55),
-                new Pemain(5, 172, 60),
-                new Pemain(6, 170, 70),
-                new Pemain(7, 169, 66),
-                new Pemain(8, 165, 56),
-                new Pemain(9, 171, 72),
-                new Pemain(10, 166, 56)
-            )
-        );
+        Map<Integer, Pemain> timA = new HashMap<>();
+        timA.put(1, new Pemain(1, 168, 50));
+        timA.put(2, new Pemain(2, 170, 60));
+        timA.put(3, new Pemain(3, 165, 56));
+        timA.put(4, new Pemain(4, 168, 55));
+        timA.put(5, new Pemain(5, 172, 60));
+        timA.put(6, new Pemain(6, 170, 70));
+        timA.put(7, new Pemain(7, 169, 66));
+        timA.put(8, new Pemain(8, 165, 56));
+        timA.put(9, new Pemain(9, 171, 72));
+        timA.put(10, new Pemain(10, 166, 56));
 
-        ArrayList<Pemain> timB = new ArrayList<>(
-            Arrays.asList(
-                new Pemain(1, 170, 66),
-                new Pemain(2, 167, 60),
-                new Pemain(3, 165, 59),
-                new Pemain(4, 166,58),
-                new Pemain(5, 168,58),
-                new Pemain(6, 175, 71),
-                new Pemain(7, 172, 68),
-                new Pemain(8, 171, 68),
-                new Pemain(9, 168, 65),
-                new Pemain(10, 169, 60)
-            )
-        );
-
-        Map<Integer, Pemain> tinggiMap = new HashMap<>();
-        Map<Integer, Pemain> beratMap = new HashMap<>();
-
-        Map<Integer, Pemain> tinggiMapB = new HashMap<>();
-        Map<Integer, Pemain> beratMapB = new HashMap<>();
-
-        // Membuat HashMap untuk kesamaan tinggi dan berat
-        for (Pemain pemain : timA) {
-            tinggiMap.put(pemain.tinggi, pemain);
-            beratMap.put(pemain.berat, pemain);
-        }
-
-        for (Pemain pemain : timB) {
-            tinggiMapB.put(pemain.tinggi, pemain);
-            beratMapB.put(pemain.berat, pemain);
-        }
-
+        Map<Integer, Pemain> timB = new HashMap<>();
+        timB.put(1, new Pemain(1, 170, 66));
+        timB.put(2, new Pemain(2, 167, 60));
+        timB.put(3, new Pemain(3, 165, 59));
+        timB.put(4, new Pemain(4, 166, 58));
+        timB.put(5, new Pemain(5, 168, 58));
+        timB.put(6, new Pemain(6, 175, 71));
+        timB.put(7, new Pemain(7, 172, 68));
+        timB.put(8, new Pemain(8, 171, 68));
+        timB.put(9, new Pemain(9, 168, 65));
+        timB.put(10, new Pemain(10, 169, 60));
         findPlayersWithSameHeight(timA, timB);
         findPlayersWithSameWeight(timA, timB);
 
@@ -72,25 +49,30 @@ public class Main {
 
         analyzePlayerWeight(timA, "A");
         analyzePlayerWeight(timB, "B");
-
     }
 
-    public static void analyzePlayerHeight(List<Pemain> team, String teamName) {
+    public static void analyzePlayerHeight(Map<Integer, Pemain> team, String teamName) {
         System.out.println("\nKategori Range Tinggi di Tim " + teamName + ":");
-        int minRangeTinggi = Collections.min(team, Comparator.comparing(p -> p.tinggi)).tinggi;
-        int maxRangeTinggi = Collections.max(team, Comparator.comparing(p -> p.tinggi)).tinggi;
+    
+        // Obtain the values from the map as a collection
+        Collection<Pemain> pemainCollection = team.values();
+    
+        // Find the minimum and maximum heights using the collection
+        int minRangeTinggi = Collections.min(pemainCollection, Comparator.comparing(p -> p.tinggi)).tinggi;
+        int maxRangeTinggi = Collections.max(pemainCollection, Comparator.comparing(p -> p.tinggi)).tinggi;
+    
         System.out.println("Tinggi Min: " + minRangeTinggi + " cm");
         System.out.println("Tinggi Max: " + maxRangeTinggi + " cm");
-
+    
         TreeMap<Integer, List<Pemain>> rangeTinggi = new TreeMap<>();
-        for (Pemain pemain : team) {
+        for (Pemain pemain : pemainCollection) {
             int tinggiGroup = pemain.tinggi / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 tinggi badan
             if (!rangeTinggi.containsKey(tinggiGroup)) {
                 rangeTinggi.put(tinggiGroup, new ArrayList<>());
             }
             rangeTinggi.get(tinggiGroup).add(pemain);
         }
-
+    
         for (Map.Entry<Integer, List<Pemain>> entry : rangeTinggi.entrySet()) {
             int tinggiGroup = entry.getKey();
             List<Pemain> pemainList = entry.getValue();
@@ -104,22 +86,28 @@ public class Main {
         }
     }
 
-    public static void analyzePlayerWeight(List<Pemain> team, String teamName) {
+    public static void analyzePlayerWeight(Map<Integer, Pemain> team, String teamName) {
         System.out.println("\nKategori Range Berat di Tim " + teamName + ":");
-        int minRangeBerat = Collections.min(team, Comparator.comparing(p -> p.berat)).berat;
-        int maxRangeBerat = Collections.max(team, Comparator.comparing(p -> p.berat)).berat;
+    
+        // Obtain the values from the map as a collection
+        Collection<Pemain> pemainCollection = team.values();
+    
+        // Find the minimum and maximum weights using the collection
+        int minRangeBerat = Collections.min(pemainCollection, Comparator.comparing(p -> p.berat)).berat;
+        int maxRangeBerat = Collections.max(pemainCollection, Comparator.comparing(p -> p.berat)).berat;
+    
         System.out.println("Berat Min: " + minRangeBerat + " kg");
         System.out.println("Berat Max: " + maxRangeBerat + " kg");
-
+    
         TreeMap<Integer, List<Pemain>> rangeBerat = new TreeMap<>();
-        for (Pemain pemain : team) {
+        for (Pemain pemain : pemainCollection) {
             int beratGroup = pemain.berat / 10 * 10; // Mengelompokkan pemain berdasarkan kelipatan 10 berat badan
             if (!rangeBerat.containsKey(beratGroup)) {
                 rangeBerat.put(beratGroup, new ArrayList<>());
             }
             rangeBerat.get(beratGroup).add(pemain);
         }
-
+    
         for (Map.Entry<Integer, List<Pemain>> entry : rangeBerat.entrySet()) {
             int beratGroup = entry.getKey();
             List<Pemain> pemainList = entry.getValue();
@@ -132,10 +120,11 @@ public class Main {
             }
         }
     }
-    private static void findPlayersWithSameHeight(List<Pemain> teamA, List<Pemain> teamB) {
-        Map<Integer, Pemain> heightMap = createHeightMap(teamA);
+    
+    private static void findPlayersWithSameHeight(Map<Integer, Pemain> teamA, Map<Integer, Pemain> teamB) {
+        Map<Integer, Pemain> heightMap = createHeightMap(teamA.values());
         System.out.println("Tinggi badan yang sama:");
-        for (Pemain pemain : teamB) {
+        for (Pemain pemain : teamB.values()) {
             if (heightMap.containsKey(pemain.tinggi)) {
                 Pemain pemainTimA = heightMap.get(pemain.tinggi);
                 System.out.println("Pemain dengan tinggi " + pemain.tinggi +
@@ -145,11 +134,11 @@ public class Main {
             }
         }
     }
-
-    private static void findPlayersWithSameWeight(List<Pemain> teamA, List<Pemain> teamB) {
-        Map<Integer, Pemain> weightMap = createWeightMap(teamA);
+    
+    private static void findPlayersWithSameWeight(Map<Integer, Pemain> teamA, Map<Integer, Pemain> teamB) {
+        Map<Integer, Pemain> weightMap = createWeightMap(teamA.values());
         System.out.println("\nBerat badan yang sama:");
-        for (Pemain pemain : teamB) {
+        for (Pemain pemain : teamB.values()) {
             if (weightMap.containsKey(pemain.berat)) {
                 Pemain pemainTimA = weightMap.get(pemain.berat);
                 System.out.println("Pemain dengan berat " + pemain.berat +
@@ -158,37 +147,35 @@ public class Main {
                         pemain.noPemain + ", " + pemain.tinggi + "cm)");
             }
         }
-    }
+    }    
 
-    private static void findPlayersWithUniqueHeight(List<Pemain> teamA, List<Pemain> teamB) {
-        Map<Integer, Pemain> heightMapB = createHeightMap(teamB);
+    private static void findPlayersWithUniqueHeight(Map<Integer, Pemain> teamA, Map<Integer, Pemain> teamB) {
         System.out.println("\nKomplemen tinggi badan tim A:");
-        for (Pemain pemain : teamA) {
-            if (!heightMapB.containsKey(pemain.tinggi)) {
+        for (Pemain pemain : teamA.values()) {
+            if (!teamB.containsKey(pemain.tinggi)) {
                 System.out.println("Pemain dengan tinggi " + pemain.tinggi + " hanya ada di Tim A");
             }
         }
     }
-
-    private static void findPlayersWithUniqueWeight(List<Pemain> teamA, List<Pemain> teamB) {
-        Map<Integer, Pemain> weightMapB = createWeightMap(teamB);
+    
+    private static void findPlayersWithUniqueWeight(Map<Integer, Pemain> teamA, Map<Integer, Pemain> teamB) {
         System.out.println("\nKomplemen berat badan tim A:");
-        for (Pemain pemain : teamA) {
-            if (!weightMapB.containsKey(pemain.berat)) {
+        for (Pemain pemain : teamA.values()) {
+            if (!teamB.containsKey(pemain.berat)) {
                 System.out.println("Pemain dengan berat " + pemain.berat + " hanya ada di Tim A");
             }
         }
     }
-
-    private static Map<Integer, Pemain> createHeightMap(List<Pemain> team) {
+    
+    private static Map<Integer, Pemain> createHeightMap(Collection<Pemain> team) {
         Map<Integer, Pemain> heightMap = new HashMap<>();
         for (Pemain pemain : team) {
             heightMap.put(pemain.tinggi, pemain);
         }
         return heightMap;
     }
-
-    private static Map<Integer, Pemain> createWeightMap(List<Pemain> team) {
+    
+    private static Map<Integer, Pemain> createWeightMap(Collection<Pemain> team) {
         Map<Integer, Pemain> weightMap = new HashMap<>();
         for (Pemain pemain : team) {
             weightMap.put(pemain.berat, pemain);
