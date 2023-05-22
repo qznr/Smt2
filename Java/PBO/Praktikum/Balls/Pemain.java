3c3b77597ff66890e6ec0f638c52ad854ea0e614
Gustav <mgustav.penting@gmail.com>
2023-05-22 09:27:19 +0700

    PBO

---
+++ Java/PBO/Praktikum/Balls/Pemain.java
@@ -0,0 +1,13 @@
+package Java.PBO.Praktikum.Balls;
+
+class Pemain {
+    int tinggi, berat, noPemain;
+
+    public Pemain(int noPemain, int tinggi, int berat) {
+        this.noPemain = noPemain;
+        this.tinggi = tinggi;
+        this.berat = berat;
+    }
+}
+
+
